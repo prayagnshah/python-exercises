cbb5bffd85be393b21f9477d9dc1d2ccd4bd6769
prayagnshah <34435248+prayagnshah@users.noreply.github.com>
2022-07-18 15:23:15 -0300

    Update Exercise_10.py

--- dictionary-exercises/Exercise_10.py
+++ dictionary-exercises/Exercise_10.py
@@ -3,10 +3,5 @@ sample_dict = {
     'emp2': {'name': 'Emma', 'salary': 8000},
     'emp3': {'name': 'Brad', 'salary': 500}
 }
-
-
 sample_dict['emp3']['salary'] = 8500    ##Adding 8500 value to emp3 in salary
-
-
-
 print(sample_dict)
