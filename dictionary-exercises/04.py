09fb6e8199e53af8260a099ad55fa27ae1bcff7a
prayagnshah <34435248+prayagnshah@users.noreply.github.com>
2022-07-22 07:58:34 -0300

    renaming dictionary exercises

---
+++ dictionary-exercises/04.py
@@ -0,0 +1,9 @@
+employees = ['Kelly', 'Emma']
+defaults = {"designation": 'Developer', "salary": 8000}
+
+res = dict.fromkeys(employees, defaults)    ##took the hint from the websiite of fromkeys() function
+print(res)
+
+
+
+
