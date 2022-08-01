3a27ca3b18b34f43bbd32196dbf01a358678d8bb
prayagnshah <34435248+prayagnshah@users.noreply.github.com>
2022-08-01 14:46:36 -0300

    corrected the function

--- string-exercises/07-revised.py
+++ string-exercises/07-revised.py
@@ -5,10 +5,10 @@ s2 = "PYnative"
 set1 = set(s1)
 set2 = set(s2)
 
-sets_intersection = set1.union(set2)
+sets_union = set1.union(set2)
 
 
 ##using this expression to print boolean expression
-is_subset = sets_intersection == set2
+is_subset = sets_union == set2
 
 print(is_subset)
--- string-exercises/18.py
+++ string-exercises/18.py
@@ -1,6 +1,13 @@
+import string
+
 str1 = "/*Jon is @developer & musician!!"
+
+
+# Replace punctuations with #
 replace_char = "#"
 
-for s in str1:
-    s.replace("/*", "#")
-    print(s)
+# string.punctuation to get the list of all special symbols
+for char in string.punctuation:
+    str1 = str1.replace(char, replace_char)
+
+print(str1)
