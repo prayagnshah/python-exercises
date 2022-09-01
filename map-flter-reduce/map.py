ad25af978cd1709b2659f2bfd1270fdc6815b100
prayagnshah <34435248+prayagnshah@users.noreply.github.com>
2022-09-01 17:53:25 -0300

    adding-practice-exercise-map

--- map-flter-reduce/map.py
+++ map-flter-reduce/map.py
@@ -45,3 +45,13 @@ for i in range(5):
     value = list(map(lambda x:x(i), func))
     print(value)
 
+##using map function and adding the numbers so concatenating int to list
+
+def sum1(num1):
+    num1 = num1 + 1
+    return num1
+
+num1 = [1,2,3,4,5,6,7]
+value1 = map(sum1, num1)
+print(list(value1))
+
