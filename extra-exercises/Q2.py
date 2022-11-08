2517e348c131d08f7dbdf61f0e5cd63385cec133
prayagnshah <34435248+prayagnshah@users.noreply.github.com>
2022-11-08 16:22:23 -0400

    fixing the global count function

--- extra-exercises/Q2.py
+++ extra-exercises/Q2.py
@@ -1,4 +1,4 @@
-##Write a python program that takes as an input, two numbers.
+# Write a python program that takes as an input, two numbers.
 # The program only accepts numbers that are greater than 0.
 # The program adds the two numbers. It asks the user to guess the result of the addition.
 # If their guess is correct a message is printed (your answer is correct), otherwise it prints (your answer is incorrect).
@@ -7,18 +7,23 @@
 # The program also prints the number of questions have been asked in the attempts.
 
 
+count = 0
+
 
 def sum_answer():
 
-    count = 0
+    global count
     count += 1
 
-    num_1 = int(input("Enter the first number: "))     ##Asking the user to enter number 1
-    num_2 = int(input('Enter the second number: '))   ##Asking the user to enter number 2
+    # Asking the user to enter number 1
+    num_1 = int(input("Enter the first number: "))
+    # Asking the user to enter number 2
+    num_2 = int(input('Enter the second number: '))
 
-    answer = int(input("What is the answer? "))     ##Asking the user to enter the answer
+    # Asking the user to enter the answer
+    answer = int(input("What is the answer? "))
 
-    ##Using if-else statement to check whether the answer of the user is right or wrong
+    # Using if-else statement to check whether the answer of the user is right or wrong
 
     if answer == num_1 + num_2:
         print("Your answer is correct.")
@@ -26,27 +31,16 @@ def sum_answer():
     else:
         print("Your answer is incorrect.")
 
-    ##Asking user if they want to try again
+    # Asking user if they want to try again
 
     ask = str(input("Do you want to try again (yes/no): "))
 
-    ##if the user say yes then we can run the loop again
+    # if the user say yes then we can run the loop again
 
     if ask == 'yes':
         print(sum_answer())
 
-    return f'The number of attempts is', count
+    return ('The number of attempts is:', count)
 
 
-
-sum_1 = print(sum_answer())  ##Calling the function
-
-
-
-# var = str(input("Do you want to try again (yes/no):" ))
-
-# if var == str('yes'):
-
-
-# print(answer)
-# print(var)
+sum_1 = print(sum_answer())  # Calling the function
