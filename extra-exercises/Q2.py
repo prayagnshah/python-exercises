83b3cc446176cd4015a1d14074f31ca710aa4fdd
prayagnshah <34435248+prayagnshah@users.noreply.github.com>
2022-11-07 23:49:39 -0400

    Practicing question 2
    Question is commented out before the solution

---
+++ extra-exercises/Q2.py
@@ -0,0 +1,52 @@
+##Write a python program that takes as an input, two numbers.
+# The program only accepts numbers that are greater than 0.
+# The program adds the two numbers. It asks the user to guess the result of the addition.
+# If their guess is correct a message is printed (your answer is correct), otherwise it prints (your answer is incorrect).
+# Then the program asks the user if they want to continue the app with a new question (use a loop for this).
+# The loop ruminates when the user answers (no).
+# The program also prints the number of questions have been asked in the attempts.
+
+
+
+def sum_answer():
+
+    count = 0
+    count += 1
+
+    num_1 = int(input("Enter the first number: "))     ##Asking the user to enter number 1
+    num_2 = int(input('Enter the second number: '))   ##Asking the user to enter number 2
+
+    answer = int(input("What is the answer? "))     ##Asking the user to enter the answer
+
+    ##Using if-else statement to check whether the answer of the user is right or wrong
+
+    if answer == num_1 + num_2:
+        print("Your answer is correct.")
+
+    else:
+        print("Your answer is incorrect.")
+
+    ##Asking user if they want to try again
+
+    ask = str(input("Do you want to try again (yes/no): "))
+
+    ##if the user say yes then we can run the loop again
+
+    if ask == 'yes':
+        print(sum_answer())
+
+    return f'The number of attempts is', count
+
+
+
+sum_1 = print(sum_answer())  ##Calling the function
+
+
+
+# var = str(input("Do you want to try again (yes/no):" ))
+
+# if var == str('yes'):
+
+
+# print(answer)
+# print(var)
